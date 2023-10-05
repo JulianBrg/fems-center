a07d52b5575318eb21f1bd3f2186830da3674063
JulianBrg <julian.barragan@alumno.uttehuacan.edu.mx>
2023-10-05 14:50:08 -0600

    remove animation to home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import './Home.scss'
 // import assets
 import background from "../../assets/back.svg";
+// import background from "../../assets/back.webp";
 import img1 from "../../assets/img6.webp";
 import img2 from "../../assets/img7.webp";
 import img3 from "../../assets/img8.webp";
@@ -9,15 +10,15 @@ import img4 from "../../assets/img9.webp";
 // imported icons
 
 // animation
-import Aos from 'aos';
+// import Aos from 'aos';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Home = () => {
 
-  useEffect(() => {
-    Aos.init({ duration: 1500 });
-  }, [])
+  // useEffect(() => {
+  //   Aos.init({ duration: 500 });
+  // }, [])
 
   return (
     <div className='Home'>
@@ -27,11 +28,11 @@ const Home = () => {
       </div>
 
       <div className="sectionText">
-        <h1 data-aos="fade-down">Fems Center</h1>
-        <h2 data-aos="fade-down">Es tu momento de crecer</h2>
-        <p data-aos="fade-down">Desarrolla tu potencial en espacios coworking y actividades para crecer profesional y personalmente.</p>
+        <h1>Fems Center</h1>
+        <h2>Es tu momento de crecer</h2>
+        <p >Desarrolla tu potencial en espacios coworking y actividades para crecer profesional y personalmente.</p>
 
-        <div data-aos="fade-down">
+        <div>
           <a
             href="https://www.facebook.com/femsenaccion"
             target="_blank"
@@ -53,8 +54,8 @@ const Home = () => {
 
       <div className="popularPlaces">
         <div className="content">
-          <h3 data-aos="fade-up">Fotos</h3>
-          <div className="images flex" data-aos="fade-up">
+          <h3>Fotos</h3>
+          <div className="images flex" >
             <img src={img1} alt="Images blog" />
             <img src={img2} alt="Images blog" />
             <img src={img3} alt="Images blog" />
