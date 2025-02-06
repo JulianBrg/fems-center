5f457d6cdaed97ccdf3459c08b0b90d76f1bc3ad
JulianBrg <julian.barragan@alumno.uttehuacan.edu.mx>
2025-02-06 12:08:51 -0600

    add rute build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
@@ -18,7 +17,9 @@ export default defineConfig({
                 },
             },
         },
+        outDir: "build",
     },
+    publicDir: "public",
     experimental: {
         appDir: true,
     },
